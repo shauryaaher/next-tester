245d9674ab303c4188d59f28570d72a08370b7e9
Shaurya Aher <shauryaaher1200@gmail.com>
2022-10-29 21:11:34 +0530

    Create a Serverless Function that fetches data from Firestore

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const path =
+  "https://firestore.googleapis.com/v1/projects/next-js-tester/databases/(default)/documents/oo/ll";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).send("Doesn't work.");
+  } else {
+    const data = await fetch(path)
+      .then((res) => res.json())
+      .catch((error) => console.error(error));
+    return res.status(200).send(Object.values(data.fields.f).toString());
+  }
 }
