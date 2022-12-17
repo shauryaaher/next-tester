3fbfedae6f3faa4a18a4b8b2968b0db15baad065
Shaurya Aher <shauryaaher1200@gmail.com>
2022-12-17 21:39:15 +0530

    Add a meta description

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,8 +7,8 @@ export async function getServerSideProps() {
   const path =
     "https://firestore.googleapis.com/v1/projects/next-js-tester/databases/(default)/documents/oo/ll";
   const req = await fetch(path)
-  .then((res) => res.json())
-  .catch((error) => console.error(error));
+    .then((res) => res.json())
+    .catch((error) => console.error(error));
   const data = await Object.values(req.fields.f).toString();
   return {
     props: {
@@ -23,13 +23,15 @@ export default function Home(props) {
       <Head>
         <title>Next.js tester project</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="This is just a Next.js demo site which fetches data from Cloud Firestore"
+        />
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>Hello world!</h1>
         <h2>Welcome to this Next.js tester project.</h2>
-        <Suspense fallback={<span>Loading...</span>}>
-          {props.text}
-        </Suspense>
+        <Suspense fallback={<span>Loading...</span>}>{props.text}</Suspense>
         <Link
           href="https://github.com/shauryaaher/next-tester"
           style={{ color: "royalblue" }}
