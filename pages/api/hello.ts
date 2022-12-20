9bcdac9e695c520859f5b9ce13d371d3c0b12b16
Shaurya Aher <72655598+shauryaaher@users.noreply.github.com>
2022-12-20 04:50:49 +0000

    Assiggn the arrow function to a variable before exporting

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,12 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export default (req: NextRequest) => {
+const func = (req: NextRequest) => {
   if (req.method === "GET") {
     const response = {hello: "hello world"};
     return NextResponse.json(Object.values(response).toString());
   } else
     return NextResponse.json("HTTP 405: Only GET requests are allowed");
 };
+
+export default func;
 
 export const config: Object = {
   runtime: "experimental-edge",
